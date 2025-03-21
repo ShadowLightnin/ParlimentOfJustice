2d2cfa5c6aac3920d4a9fe50bdd623364b63f278
Night Hawk <player1.will@gmail.com>
2025-03-20 23:13:59 -0600

    alleclipse

--- navigation/EclipseStack.js
+++ navigation/EclipseStack.js
@@ -4,6 +4,11 @@ import EclipseScreen from '../screens/Eclipse/EclipseScreen';
 import TeamChatScreen from "../screens/Eclipse/TeamChatScreen";
 
 import Aileen from '../screens/Eclipse/Aileen';
+import Myran from '../screens/Eclipse/Myran';
+import Kelsie from '../screens/Eclipse/Kelsie';
+import James from '../screens/Eclipse/James';
+
+// import {  } from '../screens//';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +18,10 @@ export function EclipseStack() {
       <Stack.Screen name="EclipseHome" component={EclipseScreen} />
       <Stack.Screen name="TeamChat" component={TeamChatScreen} />
       <Stack.Screen name="Aileen" component={Aileen} />
+      <Stack.Screen name="Myran" component={Myran} />
+      <Stack.Screen name="Kelsie" component={Kelsie} />
+      <Stack.Screen name="James" component={James} />
+
     </Stack.Navigator>
   );
 }
