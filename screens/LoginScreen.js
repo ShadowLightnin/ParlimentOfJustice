c2ab1a30259ecdf161b66d0572e81d090420241f
ShadowLightnin <player1.will@gmail.com>
2025-03-27 11:00:00 -0600

    oglogin

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -18,7 +18,6 @@ import { doc, setDoc } from 'firebase/firestore';
 import upload from '../lib/upload';
 import * as ImagePicker from 'expo-image-picker';
 import { AuthContext } from '../context/auth-context';
-import { createUserDocument, initializeChatDocument } from "../lib/firebaseUtils";
 
 const backgroundImages = [
     require('../assets/BackGround/Parliment.png'),
@@ -94,19 +93,20 @@ const LoginScreen = () => {
         try {
             const imgUrl = avatar.file ? await upload(avatar.file) : "./avatar.png";
             const res = await createUserWithEmailAndPassword(auth, email, password);
-    
-            // New Utility Usage for Cleaner Code
-            await createUserDocument(res.user.uid, {
+
+            await setDoc(doc(db, "users", res.user.uid), {
                 username,
                 email,
                 avatar: imgUrl,
+                id: res.user.uid,
+                blocked: [],
             });
-    
-            await initializeChatDocument(res.user.uid);
-    
+
+            await setDoc(doc(db, "userchats", res.user.uid), { chats: [] });
+
             const token = await res.user.getIdToken();
             authCtx.authenticate(token);
-    
+
             Toast.show({ type: 'success', text1: 'Success', text2: 'Account created! Welcome!' });
         } catch (err) {
             console.error(err);
@@ -115,7 +115,7 @@ const LoginScreen = () => {
             setLoading(false);
         }
     };
-    
+
     const handleLogin = async () => {
         setLoading(true);
         try {
