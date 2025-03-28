ee13cbda64203270256eb9bc5ff2888ffc39fa79
ShadowLightnin <player1.will@gmail.com>
2025-03-28 11:02:08 -0600

    Teachers

--- screens/Constollation/ConstollationMembers.js
+++ screens/Constollation/ConstollationMembers.js
@@ -1,33 +1,49 @@
 import constollationImages from './ConstollationImages';
 
-// 🎯 Base Constollation Members
-const baseConstollation = [
-  { name: 'Leader Name', codename: 'Legion Alpha', screen: 'Leader', clickable: false, position: [0, 0] },
-  { name: 'Member 1', codename: 'Legion Beta', screen: 'Member1', clickable: false, position: [0, 2] },
-  { name: 'Member 2', codename: 'Legion Gamma', screen: 'Member2', clickable: false, position: [1, 0] },
-  { name: 'Member 3', codename: 'Legion Delta', screen: 'Member3', clickable: false, position: [1, 1] },
-  { name: 'Member 4', codename: 'Legion Epsilon', screen: 'Member4', clickable: false, position: [1, 2] },
+// 🎯 Legionaires Member List
+const moreMembers = [
+  // Elementary
+  'Mrs. Gilbert (2nd Grade)',' Ms. Clawson (3rd Grade)','Mrs. MarryWeither(4th Grade)','Mrs. Brown(music)','Mrs. Anderson(6th Grade)','Ms. (6th Grade)', 
+  // Jr. High 7th
+  'Chad King','Mr. Chandler','Mr. Allred','Mrs. Cox','','','','','','','','','','',
+    // Jr. High 8th
+  '','','','','','','','','','','','','','','','','','','',
+    // Jr. High 9th
+  '','','','','','','','','','','','','','','','','','','',
+    // High School 10th
+  'Tood Campbell','Roxey Caternzaro','Zak Erickson','Tana Johnson','Mrs Perry','','','','','','','','','','','','','','',
+      // High School 11th
+  'Marc Allen','Lisa McLaws','Sharee Paxton','','','','','','','','','','','','','','','','',
+      // High School 12th
+  'Weston Kidder','David Pilkington','Nate Hillyard','Kori Schriver','Jack Hattaway','','','','','',
+  // College
+  'Carey Anson','Sean Fears','John Bitter','','Sarina Sinatra','','George Ray','Brent','Claudine','Braiden',
+  // Influencers
+  '','','','','','','','','','','','','','','','','','','',
+
+'Sheryl','Krystal','Deb',
 ];
 
-// 🎯 Generate additional members up to 100
-const moreMembers = Array.from({ length: 100 }, (_, i) => ({
-  name: `Member ${i + 5}`,
-  codename: `Legion ${[
-      'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega',
-      'Alpha Prime', 'Beta Prime', 'Gamma Prime', 'Delta Prime', 'Epsilon Prime', 'Zeta Prime', 'Eta Prime', 'Theta Prime', 'Iota Prime', 'Kappa Prime',
-      'Lambda Prime', 'Mu Prime', 'Nu Prime', 'Xi Prime', 'Omicron Prime', 'Pi Prime', 'Rho Prime', 'Sigma Prime', 'Tau Prime', 'Upsilon Prime', 'Phi Prime',
-      'Chi Prime', 'Psi Prime', 'Omega Prime', 'Alpha Ultra', 'Beta Ultra', 'Gamma Ultra', 'Delta Ultra', 'Epsilon Ultra', 'Zeta Ultra', 'Eta Ultra', 'Theta Ultra',
-      'Iota Ultra', 'Kappa Ultra', 'Lambda Ultra', 'Mu Ultra', 'Nu Ultra', 'Xi Ultra', 'Omicron Ultra', 'Pi Ultra', 'Rho Ultra', 'Sigma Ultra', 'Tau Ultra',
-      'Upsilon Ultra', 'Phi Ultra', 'Chi Ultra', 'Psi Ultra', 'Omega Ultra'
-  ][i] || 'Unknown'}`,
-  screen: `Member${i + 5}`,
-  position: [Math.floor((i + 5) / 3), (i + 5) % 3],
-}));
+// 🎯 Codename Mapping
+const codenameList = [ 'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 
+  'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega', 'Alpha Prime', 'Beta Prime', 'Gamma Prime', 'Delta Prime', 'Epsilon Prime', 'Zeta Prime', 'Eta Prime', 
+  'Theta Prime', 'Iota Prime', 'Kappa Prime', 'Lambda Prime', 'Mu Prime', 'Nu Prime', 'Xi Prime', 'Omicron Prime', 'Pi Prime', 'Rho Prime', 'Sigma Prime', 'Tau Prime', 
+  'Upsilon Prime', 'Phi Prime', 'Chi Prime', 'Psi Prime', 'Omega Prime', 'Alpha Ultra', 'Beta Ultra', 'Gamma Ultra', 'Delta Ultra', 'Epsilon Ultra', 'Zeta Ultra', 'Eta Ultra', 
+  'Theta Ultra', 'Iota Ultra', 'Kappa Ultra', 'Lambda Ultra', 'Mu Ultra', 'Nu Ultra', 'Xi Ultra', 'Omicron Ultra', 'Pi Ultra', 'Rho Ultra', 'Sigma Ultra', 'Tau Ultra',
+  'Upsilon Ultra', 'Phi Ultra', 'Chi Ultra', 'Psi Ultra', 'Omega Ultra',
+];
 
-// 🎯 Combine both base and additional members and map images and clickable status
-const fullConstollationList = [...baseConstollation, ...moreMembers].map(member => ({
-  ...member,
-  ...constollationImages[member.name] || { image: require('../../assets/Armor/PlaceHolder.jpg'), clickable: false },
-}));
+// 🎯 Generate member list with image and clickable status from legionImages
+const fullConstollationList = moreMembers.map((name, i) => {
+  const memberData = constollationImages[name] || { image: require('../../assets/Armor/PlaceHolder.jpg'), clickable: false };
+
+  return {
+      name,
+      codename: `Legion ${codenameList[i] || 'Unknown'}`,  // 🔥 Added proper codename format
+      screen: `Member${i + 1}`,
+      position: [Math.floor(i / 3), i % 3],
+      ...memberData,
+  };
+});
 
 export default fullConstollationList;
--- screens/Legionaires/LegionairesMembers.js
+++ screens/Legionaires/LegionairesMembers.js
@@ -5,27 +5,36 @@ const baseLegionaires = [];
 
 // 🎯 Legionaires Member List
 const moreMembers = [
-    'Mason Erickson','Davis Clark','Cody Cline','Kinnley Cline','Cassidy Cline','Jack Smith','Dylan',
-    'Jessica Fisher','Dustin Edmonds','Justin','Zack','Josh Noble','Will Knight','Christian','John','Jose','Matt L','Anthony','Miles','Cieanna',
-    'Natalie','Kanyon','Emily','Jarom','Avery','Emily','Kennedy Carman','Sydney','Hunter','Cami Webb','Spencer Hill',
-    'Clayton','Spencer L','Malia Holley','Mika','Jason','Allysa','Josh','John 1','John 2','Abby','Tagun','Adam Ledger',
-    'Sam','Kaden Hanson 1','Sam Philops','Carter Olsen','Issac Philops','Hayden','Hayden','Parker Gold','Gabe Parker',
+    'Mason Erickson','Davis Clark','Josh Noble','Spencer Hill','Justin','Zack','Will Knight','Cody Cline','Kinnley Cline','Cassidy Cline',
+    'Jessica Fisher','Dylan','Kennedy Carman','Jack','Allysa','Matt Lephi','Jack Smith','Dustin Edmonds','Jose Rodregos',
+    'Anthony','Miles','Cieanna','Natalie','Kanyon','Emily','Jarom','Avery','Emily','Sydney','Hunter','Cami Webb',
+    'Clayton','Spencer L','Malia Holley','Clayton','Spencer L','Sam','Malia Holley','Mika','Jason','Allysa','Josh','Abby','Tagun','Adam Ledger',
+    'Kaden Hanson 1','Sam Philops','Carter Olsen','Issac Philops','Hayden','Hayden','Parker Gold','Gabe Parker','Christian','John',
     'Sanford Duncan ','Lizzy','Kyle','Ivan','Grace','Kaden Hansen 2','Kaia','Erica','Veronica','Elizabeth','Shanna','Lupa',
     'Kody','Nate Greene','Nick','Thomas','Kason','Bridger','Abby','Addie','Brigdon','Noah','Elsie','Mason','Emery Clark',
-    'Xzavier','Quinton','Quinton C','Quincy',
-  ];
+    'Xzavier','Quinton','Quinton C','Quincy','Sheryl','Krystal','Deb',
+];
+
+// 🎯 Codename Mapping
+const codenameList = [ 'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 
+    'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega', 'Alpha Prime', 'Beta Prime', 'Gamma Prime', 'Delta Prime', 'Epsilon Prime', 'Zeta Prime', 'Eta Prime', 
+    'Theta Prime', 'Iota Prime', 'Kappa Prime', 'Lambda Prime', 'Mu Prime', 'Nu Prime', 'Xi Prime', 'Omicron Prime', 'Pi Prime', 'Rho Prime', 'Sigma Prime', 'Tau Prime', 
+    'Upsilon Prime', 'Phi Prime', 'Chi Prime', 'Psi Prime', 'Omega Prime', 'Alpha Ultra', 'Beta Ultra', 'Gamma Ultra', 'Delta Ultra', 'Epsilon Ultra', 'Zeta Ultra', 'Eta Ultra', 
+    'Theta Ultra', 'Iota Ultra', 'Kappa Ultra', 'Lambda Ultra', 'Mu Ultra', 'Nu Ultra', 'Xi Ultra', 'Omicron Ultra', 'Pi Ultra', 'Rho Ultra', 'Sigma Ultra', 'Tau Ultra',
+    'Upsilon Ultra', 'Phi Ultra', 'Chi Ultra', 'Psi Ultra', 'Omega Ultra',
+];
 
 // 🎯 Generate member list with image and clickable status from legionImages
 const generatedMembers = moreMembers.map((name, i) => {
-  const memberData = legionImages[name] || { image: require('../../assets/Armor/PlaceHolder.jpg'), clickable: false };
+    const memberData = legionImages[name] || { image: require('../../assets/Armor/PlaceHolder.jpg'), clickable: false };
 
-  return {
-    name,
-    codename: 'Legion Member',
-    screen: `Member${i + 1}`,
-    position: [Math.floor(i / 3), i % 3],
-    ...memberData,
-  };
+    return {
+        name,
+        codename: `Legion ${codenameList[i] || 'Unknown'}`,  // 🔥 Added proper codename format
+        screen: `Member${i + 1}`,
+        position: [Math.floor(i / 3), i % 3],
+        ...memberData,
+    };
 });
 
 export default generatedMembers;
