4bcd48d80fde64867ff6eaffcdb0836f302041c5
Night Hawk <player1.will@gmail.com>
2025-04-17 11:44:39 -0600

    better doc

--- screens/Constollation/ConstollationImages.js
+++ screens/Constollation/ConstollationImages.js
@@ -1,6 +1,6 @@
 // 🎯 Static mapping of character images and clickable status for Constollation members
 const constollationImages = {
-  'Beckstead': { image: require('../../assets/Armor/DocBecksteadPlaceHolder.jpg'), clickable: true },
+  'Beckstead': { image: require('../../assets/Armor/DocBecksteadPlaceHolder2.jpg'), clickable: true },
   'George Ray': { image: require('../../assets/Armor/GeorgePlaceHolder.jpg'), clickable: true },
   'Brent': { image: require('../../assets/Armor/BrentPlaceHolder.jpg'), clickable: true },
   'Claudine': { image: require('../../assets/Armor/ClaudinePlaceHolder.jpg'), clickable: true },
