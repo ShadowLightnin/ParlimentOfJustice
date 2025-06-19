9d518830cf1adc7cde359c345f844987548a7ba8
Night Hawk <player1.will@gmail.com>
2025-06-18 19:11:19 -0600

    Dog

--- screens/Olympians/OlympiansMembers.js
+++ screens/Olympians/OlympiansMembers.js
@@ -277,6 +277,8 @@ const characterImages = {
   Kris: require('../../assets/Armor/KrisPlaceHolder.jpg'),
   Carlo: require('../../assets/Armor/CarloPlaceHolder.jpg'),
   'Cham Cham Kaleigha': require('../../assets/Armor/ChamPlaceHolder.jpg'),
+  'Molly': require('../../assets/Armor/MollyPlaceHolder.jpg'),
+  'Lumi': require('../../assets/Armor/LumiPlaceHolder.jpg'),
 };
 
 // 🎯 Generate additional members with family names and superhero codenames
