d56953b2d12e101be68f4a21325ab817454a1fac
Night Hawk <player1.will@gmail.com>
2025-06-23 16:00:43 -0600

    young womens

--- screens/Legionaires/LegionairesImages.js
+++ screens/Legionaires/LegionairesImages.js
@@ -3,6 +3,34 @@ const legionImages = {
   'Mason Erickson': { image: require('../../assets/Armor/MasonEPlaceHolder.jpg'), clickable: true },
   'Davis Clark': { image: require('../../assets/Armor/DavisPlaceHolder.jpg'), clickable: true },
   'Mckain Bailey': { image: require('../../assets/Armor/BaileyPlaceHolder.jpg'), clickable: true },
+
+  'Mika': { image: require('../../assets/Armor/MikaPlaceHolder.jpg'), clickable: true },
+  'Malia': { image: require('../../assets/Armor/MaliaPlaceHolder.jpg'), clickable: true },
+  'Jami': { image: require('../../assets/Armor/JamiPlaceHolder.jpg'), clickable: true },
+  'Cami': { image: require('../../assets/Armor/CamiPlaceHolder.jpg'), clickable: true },
+  'Kanani': { image: require('../../assets/Armor/KananiPlaceHolder.jpg'), clickable: true },
+
+
+  // 'Kenndedy Carman': { image: require('../../assets/Armor/KennedyPlaceHolder.jpg'), clickable: true },
+  // 'Jack Clark': { image: require('../../assets/Armor/JackCPlaceHolder.jpg'), clickable: true },
+  // 'Emma Bronson': { image: require('../../assets/Armor/EmmaBPlaceHolder.jpg'), clickable: true },
+  // 'Ella Duce': { image: require('../../assets/Armor/EllaPlaceHolder.jpg '), clickable: true },
+  // 'Addison Morris': { image: require('../../assets/Armor/AddisonPlaceHolder.jpg'), clickable: true },
+  // 'Alyssa': { image: require('../../assets/Armor/AlyssaPlaceHolder.jpg'), clickable: true },
+  // 'Josh Clark': { image: require('../../assets/Armor/ClarkPlaceHolder.jpg'), clickable: true },
+  // 'Miles Robinson': { image: require('../../assets/Armor/MilesPlaceHolder.jpg'), clickable: true },
+  // 'Lizzy': { image: require('../../assets/Armor/LizzyPlaceHolder.jpg'), clickable: true },
+  // 'Sam': { image: require('../../assets/Armor/SamPlaceHolder.jpg'), clickable: true },
+  // 'Spencer': { image: require('../../assets/Armor/SpencerPlaceHolder.jpg'), clickable: true },
+  // 'Sanfor Duncan': { image: require('../../assets/Armor/SanforPlaceHolder.jpg'), clickable: true },
+  // 'Magic Matt': { image: require('../../assets/Armor/MagicMattPlaceHolder.jpg'), clickable: true },
+  // 'John': { image: require('../../assets/Armor/JohnPlaceHolder.jpg'), clickable: true },  
+  // 'Emery': { image: require('../../assets/Armor/EmeryPlaceHolder.jpg'), clickable: true },
+  // 'Mason': { image: require('../../assets/Armor/MasonPlaceHolder.jpg'), clickable: true },
+  // 'Xavier': { image: require('../../assets/Armor/XavierPlaceHolder.jpg'), clickable: true },
+  // 'Josh Davis': { image: require('../../assets/Armor/JoshDPlaceHolder.jpg'), clickable: true },
+  
+
   'Cody Cline': { image: require('../../assets/Armor/CodyPlaceHolder.jpg'), clickable: true },
   'Kinnley Cline': { image: require('../../assets/Armor/KinnleyPlaceHolder.jpg'), clickable: true },
   'Cassidy Cline': { image: require('../../assets/Armor/CassidyPlaceHolder.jpg'), clickable: true },
@@ -10,13 +38,18 @@ const legionImages = {
   Dylan: { image: require('../../assets/Armor/DylanPlaceHolder.jpg'), clickable: true },
   'Jessica Fisher': { image: require('../../assets/Armor/JessicaFPlaceHolder.jpg'), clickable: true },
   'Dustin Edmonds': { image: require('../../assets/Armor/DustinEPlaceHolder.jpg'), clickable: true },
-  // 'Justin Platt': { image: require('../../assets/Armor/JustinPlaceHolder2.jpg'), clickable: true },
-  // 'Zack Dustin': { image: require('../../assets/Armor/ZackPlaceHolder2_cleanup.jpg'), clickable: true },
+  'Justin Platt': { image: require('../../assets/Armor/JustinPlaceHolder2.jpg'), clickable: true },
+  'Zack Dustin': { image: require('../../assets/Armor/ZackPlaceHolder2_cleanup.jpg'), clickable: true },
+
+
   'Josh Noble': { image: require('../../assets/Armor/JoshNoblePlaceHolder.jpg'), clickable: true },
   'Will Knight': { image: require('../../assets/Armor/WillKPlaceHolder.jpg'), clickable: true },
+
+
   Abby: { image: require('../../assets/Armor/AbbyGhostPlaceHolder.jpg'), clickable: true },
   'Veronica Teaford': { image: require('../../assets/Armor/VeronicaPlaceHolder.jpg'), clickable: true },
   'Erica Teaford': { image: require('../../assets/Armor/EricaPlaceHolder.jpg'), clickable: true },
+
 
   Sheryl: { image: require('../../assets/Armor/ShePlaceHolder.jpg'), clickable: true },
   Krystel: { image: require('../../assets/Armor/KrystelPlaceHolder.jpg'), clickable: true },
