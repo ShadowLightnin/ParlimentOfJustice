c89f862bd4bf0e4cf5900e1363fe06eb47c4d410
Night Hawk <player1.will@gmail.com>
2025-07-04 22:09:58 -0600

    sam access

--- screens/Villains/DemonMembers/Oni.js
+++ screens/Villains/DemonMembers/Oni.js
@@ -39,7 +39,7 @@ const horizontalSpacing = isDesktop ? 40 : 20;
 const verticalSpacing = isDesktop ? 50 : 20;
 
 // Permissions
-const ALLOWED_EMAILS = ["will@test.com", "c1wcummings@gmail.com"];
+const ALLOWED_EMAILS = ["will@test.com", "c1wcummings@gmail.com", "samuelp.woodwell@gmail.com"];
 const RESTRICT_ACCESS = true;
 
 const OniScreen = () => {
