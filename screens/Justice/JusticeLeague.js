82a7fd606c4a7ce1c36b6fc83ff9b5ea4728550d
Night Hawk <player1.will@gmail.com>
2025-08-05 21:19:56 -0600

    league

--- screens/Justice/JusticeLeague.js
+++ screens/Justice/JusticeLeague.js
@@ -1,5 +1,13 @@
 const JusticeLeague = [
+    { name: 'Starman', screen: '', image: require('../../assets/Armor/JusticeLeague/Starman.jpg'), clickable: true },
     { name: 'Dragoknight', screen: '', image: require('../../assets/Armor/JusticeLeague/Dragoknight.jpg'), clickable: true },
+    { name: 'Flare', screen: '', image: require('../../assets/Armor/JusticeLeague/Flare.jpg'), clickable: true },
+    { name: 'Vindra', screen: '', image: require('../../assets/Armor/JusticeLeague/Vindra.jpg'), clickable: true },
+    { name: '', screen: '', image: require('../../assets/Armor/JusticeLeague/IMG1.jpg'), clickable: true },
+    { name: '', screen: '', image: require('../../assets/Armor/JusticeLeague/IMG2.jpg'), clickable: true },
+    { name: '', screen: '', image: require('../../assets/Armor/JusticeLeague/IMG3.jpg'), clickable: true },
+    { name: '', screen: '', image: require('../../assets/Armor/JusticeLeague/IMG4.jpg'), clickable: true },
+    { name: '', screen: '', image: require('../../assets/Armor/JusticeLeague/IMG5.jpg'), clickable: true },
     // { name: 'The Flash', screen: '', image: require('../../assets/Armor/JusticeLeague/TheFlash.jpg'), clickable: true },
 ];
 
